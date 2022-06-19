a5c64d584be6a24036afd912e201bf30dedebc8f
unknown <Jahongir Yo'ldoshev>
2022-06-19 07:02:40 -0700

    start

---
+++ src/main/java/com/example/warehouseuz/WarehouseUzApplication.java
@@ -0,0 +1,13 @@
+package com.example.warehouseuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseUzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseUzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/warehouseuz/WarehouseUzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warehouseuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseUzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
